d17c73cea4796d6581e0fb539ece6b97d9ca3c38
Gustavo <gustavo.it@outlook.com>
2021-07-29 16:18:21 -0300

    Feat
    1. Configurado marcação de tarefas completadas.
    2. Configurada função para deletar tarefas.
    3. Configurado função para adicionar nova tarefa.
    4. Todos os testes passaram com sucesso.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,47 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [emptyTask, setEmptyTask] = useState(false)
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle === '') return setEmptyTask(true)
+    
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    const tarefas = [...tasks, newTask]
+
+    setTasks(tarefas)
+    setEmptyTask(false)
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const newTaskState = tasks.map((task: { id: number; isComplete: boolean; }) => {
+      if(task.id === id) {
+        task.isComplete = (!task.isComplete)
+      }
+      
+      return task;
+
+    })
+
+    setTasks(newTaskState)
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const tasksWithoutRemovedTask = tasks.filter((task: { id: number; }) => task.id !== id)
+
+    setTasks(tasksWithoutRemovedTask)
   }
 
   return (
@@ -32,12 +62,13 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
-            onChange={(e) => setNewTaskTitle(e.target.value)}
-            value={newTaskTitle}
-          />
+        
+            <input 
+              type="text" 
+              placeholder="Adicionar novo todo" 
+              onChange={(e) => setNewTaskTitle(e.target.value)}
+              value={newTaskTitle}
+            />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
             <FiCheckSquare size={16} color="#fff"/>
           </button>
@@ -60,6 +91,7 @@ export function TaskList() {
                 </label>
                 <p>{task.title}</p>
               </div>
+             
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
                 <FiTrash size={16}/>
@@ -69,6 +101,8 @@ export function TaskList() {
           
         </ul>
       </main>
+      {emptyTask && <span style={{color: 'red', fontSize: '.7rem', marginRight: '.5rem'}}>Adicione um nome a sua task</span>}
     </section>
+ 
   )
 }
